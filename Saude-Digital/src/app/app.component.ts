1ea842f350109148c4cdc0ef0e022f1ae20fa0b5
carlosalbertoosf <carlosalbertoosf@gmail.com>
2024-12-11 19:17:24 -0300

    Corrigindo app.component.html

--- Saude-Digital/src/app/app.component.ts
+++ Saude-Digital/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-
 import { NavbarComponent } from './navbar/navbar.component';
 import { DigitalSSectionComponent } from './digital-s-section/digital-s-section.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { FormAtendimentoComponent } from './form-atendimento/form-atendimento.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ NavbarComponent, DigitalSSectionComponent, RodapeComponent, FormAtendimentoComponent],
+  imports: [ NavbarComponent, DigitalSSectionComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
